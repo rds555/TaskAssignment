7285bdd7c93cee55e3a2c8da55d38a513cda9139
rds555 <59278293+rds555@users.noreply.github.com>
2021-05-06 16:26:54 +0530

    Update LoginDAOImpl.java
    Changed to callable statements

--- src/com/app/dao/LoginDAOImpl.java
+++ src/com/app/dao/LoginDAOImpl.java
@@ -11,15 +11,18 @@ public class LoginDAOImpl implements LoginDAO{
 
 	@Override
 	public String loginCheck(Login loginBean) {
-		String query="select * from tbl_login where email=? and password=?";
-		
+		//String query="select * from tbl_login where email=? and password=?";
+		String plsql="CALL getlogin(?,?)";
 		try{
 			Connection con=DBConnectionUtil.openConnection();
-			PreparedStatement ps=con.prepareStatement(query);
-			ps.setString(1,loginBean.getEmail());
-			ps.setString(2,loginBean.getPassword());
-			
-			ResultSet rs=ps.executeQuery();
+			//PreparedStatement ps=con.prepareStatement(query);
+			//ps.setString(1,loginBean.getEmail());
+			//ps.setString(2,loginBean.getPassword());
+			CallableStatement cs= con.prepareCall(plsql);
+			cs.setString(1,loginBean.getEmail() );
+			cs.setString(2,loginBean.getPassword() );
+			ResultSet rs=cs.executeQuery();
+			//ResultSet rs=ps.executeQuery();
 			
 			if(rs.next()){
 				return "true";
